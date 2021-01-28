5f38e7c6bd45aefca5ec13a84eabf9071b9ca66a
Manuel Alejandro Daza <manuel.daza@alkosto.com.co>
2021-01-28 15:44:57 -0500

    Se corriguen las fleclas de categorias

--- Kalley/js/home.js
+++ Kalley/js/home.js
@@ -3,12 +3,13 @@ let akHomeElementsQty = akHomeElements.length;
 let akHomeHolder = document.getElementsByClassName('k-categories-holder');
 let akHomeElementList = document.getElementsByClassName('k-categories-list')[0];
 let akHomeStep = 20;
+
 function akHomeResize() {
     let holderWidth = akHomeHolder[0].offsetWidth;
-    if ( window.matchMedia( '(min-width: 75rem)' ).matches ) {
+    if (window.matchMedia('(min-width: 75rem)').matches) {
         let newWidth = holderWidth * .2;
         akHomeStep = newWidth;
-        for ( let i = 0; i < akHomeElementsQty; i++ ) {
+        for (let i = 0; i < akHomeElementsQty; i++) {
             akHomeElements[i].style.flex = '0 0 ' + newWidth + 'px';
         }
     }
@@ -18,26 +19,27 @@ akHomeResize();
 
 
 akHomeElementList.style.left = '0px';
-document.querySelector('.k-categories-holder a.left').addEventListener('click', function (e) {
+document.querySelector('.k-categories-holder a.right').addEventListener('click', function(e) {
     e.preventDefault();
-    let newLeft = parseFloat( akHomeElementList.style.left ) - akHomeStep;
-    let limit = -1 * ( akHomeHolder[0].offsetWidth - ( akHomeHolder[0].offsetWidth * .2 ));
-    if ( newLeft < limit ) {
+    let newLeft = parseFloat(akHomeElementList.style.left) - akHomeStep;
+    let limit = -1 * (akHomeHolder[0].offsetWidth - (akHomeHolder[0].offsetWidth * .2));
+    if (newLeft < limit) {
         newLeft = limit
     }
-    akHomeMoveList( newLeft );
+    akHomeMoveList(newLeft);
 
 
 });
-document.querySelector('.k-categories-holder a.right').addEventListener('click', function (e) {
+document.querySelector('.k-categories-holder a.left').addEventListener('click', function(e) {
     e.preventDefault();
-    let newLeft = parseFloat( akHomeElementList.style.left ) + akHomeStep
-    if ( newLeft > 0 ) {
+    let newLeft = parseFloat(akHomeElementList.style.left) + akHomeStep
+    if (newLeft > 0) {
         newLeft = 0;
     }
-    akHomeMoveList( newLeft );
+    akHomeMoveList(newLeft);
 
 });
-function akHomeMoveList( left ) {
+
+function akHomeMoveList(left) {
     akHomeElementList.style.left = left + 'px';
 }
